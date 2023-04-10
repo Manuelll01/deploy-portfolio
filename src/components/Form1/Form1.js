8791ad9a52e81460dc2466b64e8a736b223e7842
Mircea <negoitamanuel@yahoo.com>
2023-04-11 02:53:25 +0300

    first commit

--- src/components/Form1/Form1.js
+++ src/components/Form1/Form1.js
@@ -5,31 +5,35 @@ import "./Form1.css"
 export const Form1 = () => {
     return(
         <div className="Container-Form1">
-            <form>
+            <form action="https://formsubmit.co/manuel.mircea20@gmail.com"  method="POST">
+
+            <input type="hidden" name="_captcha" value="false"></input>
+            <input type="hidden" name="_next" value="https://manuelll01.github.io/deploy-portfolio/Thank-You-page"></input>
+
                     <div className="titlu-Form1">
                         <h2>Contactează-ne !</h2>
                     </div>
                     <div className="container-input-Form1"  id="input1-Form1">
-                        <input type='text' required='required' ></input>
+                        <input type='text' required='required' name="Nume:" ></input>
                         <span><i class="fa-solid fa-user"></i><p>Nume*</p></span>
                     </div>
                      <div className="container-input-Form1" id="input2-Form1">
-                        <input type='text' required='required'  ></input>
+                        <input type='text' required='required' name="E-mail:"  ></input>
                         <span><i class="fa-solid fa-envelope"></i><p>E-mail*</p></span>
                     </div>
                     <div className="aceasi-linie">
                         <div className="container-input-Form1" id="input3-Form1">
-                            <input type='text' required='required'  ></input>
+                            <input type='text' required='required' name="Număr de telefon:" ></input>
                             <span><i class="fa-solid fa-phone"></i><p>Telefon*</p></span>
                         </div>
                         <div className="container-input-Form1" id="input4-Form1">
-                            <input type='text' required='required'  ></input>
+                            <input type='text' required='required' name="Cod Poștal:" ></input>
                             <span><i class="fa-solid fa-envelopes-bulk"></i><p>Cod Poștal*</p></span>
                         </div>
                     </div>
                     
                     <div className="container-input-Form1" id="text-area">
-                        <textarea className="text" type='text' required='required'></textarea>
+                        <textarea className="text" type='text' required='required' name="mesaj" ></textarea>
                         <span><i class="fa-solid fa-message"></i><p>Mesaj</p></span>
                     </div>
                     <div className="container-input-Form1">
--- src/components/FormMic2/FormMic2.js
+++ src/components/FormMic2/FormMic2.js
@@ -4,24 +4,28 @@ import "./FormMic2.css"
 export const FormMic2 = () => {
     return(
         <div className="Container-ContactFormMic2">
-            <form>
+            <form action="https://formsubmit.co/manuel.mircea20@gmail.com"  method="POST">
+
+            <input type="hidden" name="_captcha" value="false"></input>
+            <input type="hidden" name="_next" value="https://manuelll01.github.io/deploy-portfolio/Thank-You-page"></input>
+
                     <div className="titlu-FormMic2">
                         <h2>Contactează-ne !</h2>
                     </div>
                     <div className="container-input-FormMic2"  id="input1">
-                        <input type='text' required='required' ></input>
+                        <input type='text' required='required' name="Nume:" ></input>
                         <span><i class="fa-solid fa-user"></i>Nume*</span>
                     </div>
                      <div className="container-input-FormMic2" id="input2">
-                        <input type='text' required='required'  ></input>
+                        <input type='text' required='required' name="E-mail:" ></input>
                         <span><i class="fa-solid fa-envelope"></i>E-mail*</span>
                     </div>
                     <div className="container-input-FormMic2" id="input3">
-                        <input type='text' required='required'  ></input>
+                        <input type='text' required='required' name="Număr de telefon:" ></input>
                         <span><i class="fa-solid fa-phone"></i>Telefon*</span>
                     </div>
                     <div className="container-input-FormMic2" id="text-area">
-                        <textarea className="text" type='text' required='required'></textarea>
+                        <textarea className="text" type='text' required='required' name="Mesaj:"></textarea>
                         <span><i class="fa-solid fa-phone"></i>Mesaj</span>
                     </div>
                     <div className="container-input-FormMic2">
